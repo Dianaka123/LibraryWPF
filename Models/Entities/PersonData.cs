70b8117fab9ae95bd75b49451dbb840536d874e3
Dianaka123 <diana.aristova.99@gmail.com>
2026-01-18 18:57:31 +0300

    Добавьте файлы проекта.

---
+++ Models/Entities/PersonData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Models.Entities
+{
+    public abstract class PersonData
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+
+        public virtual string GetFullName => $"{LastName} {Name} {Patronymic}".Trim();
+    }
+}
